714858db4a41f5848efdafce874aeba1f20458b5
Yaswanth Kandanagolla <117854082+Yash070803@users.noreply.github.com>
2025-03-23 14:44:54 +0000

    made-migrations-to-postgres

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = "Backend.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'Root',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
