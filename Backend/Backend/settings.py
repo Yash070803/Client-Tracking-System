e6e6e3a46604be29183badda1c4ce7e83ddae8d1
Yaswanth Kandanagolla <117854082+Yash070803@users.noreply.github.com>
2025-03-24 12:09:23 +0000

    installed-django-rest-framework_simple-jwt_added-to-settings

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'rest_framework', #added
+    'client.apps.ClientConfig', #added
+    'Tryit', #added
+    'corsheaders', #added
 ]
 
 MIDDLEWARE = [
@@ -125,3 +130,14 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME':timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME':timedelta(days=1),
+}
