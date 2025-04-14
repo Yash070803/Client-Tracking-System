28541e5a73d51a446960243fb7ed143349c42bc7
Yaswanth Kandanagolla <117854082+Yash070803@users.noreply.github.com>
2025-04-14 11:51:29 +0000

    solved-an-issue-of-Login-Error-Message

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -13,36 +13,44 @@ const Login = ({ setAuth }) => {
     e.preventDefault();
     console.log("Login handler called");
     try {
-      const res = await axios.post('http://127.0.0.1:8000/test/api/token/', {username, password});
+      const res = await axios.post('http://127.0.0.1:8000/test/api/token/', { username, password });
       localStorage.setItem('access_token', res.data.access);
       setAuth(true);
       navigate('/clients');
-    } catch (error) {setError('Invalid credentials');} //set error message on failure
+    } catch (error) { setError('Invalid credentials'); } //set error message on failure
   };
+
+  //This helps in clearing the error when I modify the input fields
+  const handleInputChange = (setter) => (e) => {
+    setter(e.target.value);
+    setError(''); //reset error on input change
+  }
 
   return (
     <div className={styles.container}>
       <div className={styles.loginBox}>
         <h2 className={styles.title}>Login</h2>
-        <div className={styles.inputGroup}>
-          <input
-            className={styles.input}
-            value={username}
-            onChange={e => setUsername(e.target.value)}
-            placeholder="Username"
-          />
-        </div>
-        <div className={styles.inputGroup}>
-          <input
-            className={styles.input}
-            type="password"
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-            placeholder="Password"
-          />
-        </div>
-        <button className={styles.button} onClick={handleLogin}>Login</button>
-        {error && <span className={styles.error}>{error}</span>}
+        <form onSubmit={handleLogin}>
+          <div className={styles.inputGroup}>
+            <input
+              className={styles.input}
+              value={username}
+              onChange={handleInputChange(setUsername)}
+              placeholder="Username"
+            />
+          </div>
+          <div className={styles.inputGroup}>
+            <input
+              className={styles.input}
+              type="password"
+              value={password}
+              onChange={handleInputChange(setPassword)}
+              placeholder="Password"
+            />
+          </div>
+          <button className={styles.button} onClick={handleLogin}>Login</button>
+          {error && <span className={styles.error}>{error}</span>}
+        </form>
       </div>
     </div>
   );
